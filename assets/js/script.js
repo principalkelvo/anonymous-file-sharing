e2c3ed0da2cb7b7ff7ac312ff4ff653b4a94ac8a
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-22 09:42:59 +0300

    imports variables from html

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Select your form for submition
+form = document.getElementById("form");
+
+//LOADER
+let isLoading = false; 
+
+// Select your input type file and store it in a variable
+let input = document.querySelector('input[type="file"]');
+
