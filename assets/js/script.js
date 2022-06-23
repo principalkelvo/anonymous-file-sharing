ebbd687f7404452aa722f7756034a481c22877ad
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-23 17:46:47 +0300

    adds event listener for copyText id formats the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,16 @@ let form = document.getElementById("form");
 let file;
 let link;
 
-
 // Select your input type file and store it in a variable
-let input = document.getElementById('file');
+let input = document.getElementById("file");
 
 //UPLOAD FILE and send response to a variable
 //browse file
 const button = document.getElementById("browseFile");
-button.onclick = () => input.click();//on click open file selector
+button.onclick = () => input.click(); //on click open file selector
 
 // submit file button
-const submitBtn= document.getElementById("submit-btn");
-
-
-
+const submitBtn = document.getElementById("submit-btn");
 
 //submit the form
 form.addEventListener("submit", async function (event) {
@@ -32,59 +28,58 @@ form.addEventListener("submit", async function (event) {
   data.append("file", file);
   console.log(data);
 
-  await fetch("https://file.io", { // Your POST endpoint
+  await fetch("https://file.io", {
+    // Your POST endpoint
     method: "POST", // or 'PUT'
     headers: {
-        /*One needs to omit content-type header for the Fetch request. 
+      /*One needs to omit content-type header for the Fetch request. 
         Then the browser will automatically add the Content type header including the Form Boundary which looks like:
          "Content-Type": 'multipart/form-data; boundary=—-WebKitFormBoundaryfgtsKTYLsT7PNUVD',*/
     },
-    body: data,// This is your file object
+    body: data, // This is your file object
   })
-    .then((response) => response.json())// if the response is a JSON object
+    .then((response) => response.json()) // if the response is a JSON object
     .then((result) => {
       console.log("Success:", result); // Handle the success response object
-      if (result) {link = result.link; 
-        progressBar(); 
-        linkView();} // Handle the success response object}
-
+      if (result) {
+        link = result.link;
+        progressBar();
+        linkView();
+      } // Handle the success response object}
     })
     .catch((error) => {
-      console.error("Error:", error);// Handle the error response object
-      
+      console.error("Error:", error); // Handle the error response object
+      alert("Error! Try again later");
     });
-
 });
-
 
 //drag and drop
 const dropArea = document.querySelector(".drag-area"),
-dragText = dropArea.querySelector(".dragHeader");
+  dragText = dropArea.querySelector(".dragHeader");
 
-
-input.addEventListener("change", function(){
+input.addEventListener("change", function () {
   //getting user select file and [0] this means if user select multiple files then we'll select only the first one
   file = input.files[0];
   dropArea.classList.add("active");
-  console.log(file)
+  console.log(file);
   showFile(); //calling function
 });
 
 //If user Drag File Over DropArea
-dropArea.addEventListener("dragover", (event)=>{
+dropArea.addEventListener("dragover", (event) => {
   event.preventDefault(); //preventing from default behaviour
   dropArea.classList.add("active");
   dragText.textContent = "Release to Upload File";
 });
 
 //If user leave dragged File from DropArea
-dropArea.addEventListener("dragleave", ()=>{
+dropArea.addEventListener("dragleave", () => {
   dropArea.classList.remove("active");
   dragText.textContent = "Drag & Drop to Upload File";
 });
 
 //If user drop File on DropArea
-dropArea.addEventListener("drop", (event)=>{
+dropArea.addEventListener("drop", (event) => {
   event.preventDefault(); //preventing from default behaviour
   //getting user select file and [0] this means if user select multiple files then we'll select only the first one
   file = event.dataTransfer.files[0];
@@ -92,21 +87,22 @@ dropArea.addEventListener("drop", (event)=>{
 });
 
 //file preview
-function showFile(){
+function showFile() {
   let fileType = file.type; //getting selected file type
   let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
-  if(validExtensions.includes(fileType)){ //if user selected file is an image file
+  if (validExtensions.includes(fileType)) {
+    //if user selected file is an image file
     let fileReader = new FileReader(); //creating new FileReader object
-    fileReader.onload = ()=>{
+    fileReader.onload = () => {
       let fileURL = fileReader.result; //passing user file source in fileURL variable
       let imgTag = `<img src="${fileURL}" alt="">`; //creating an img tag and passing user selected file source inside src attribute
       dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
-    }
+    };
     fileReader.readAsDataURL(file);
-    console.log(file); 
+    console.log(file);
     // input =file;
-    console.log('hello file'); 
-  }else{
+    console.log("hello file");
+  } else {
     alert("This is not an Image File!");
     dropArea.classList.remove("active");
     dragText.textContent = "Drag & Drop to Upload File";
@@ -114,43 +110,50 @@ function showFile(){
 }
 
 //progressBar
-function progressBar(){
- let progressIndicator= document.querySelector(".progressIndicator");
+function progressBar() {
+  let progressIndicator = document.querySelector(".progressIndicator");
   progressIndicator.style.animation = "progress 6s";
   progressIndicator.style.width = "100%";
-  setTimeout(function () {document.querySelector(".close").style.display = "none";
-  document.querySelector(".success").style.display = "block"; }, 6000);
+  setTimeout(function () {
+    document.querySelector(".close").style.display = "none";
+    document.querySelector(".success").style.display = "block";
+  }, 6000);
   progressBarPercent();
 }
 
 function progressBarPercent() {
-let progressValue = 0
-let progressText= document.querySelector('.progressPerc')
-let progress = setInterval(() => {
-  progressValue++;
-  progressText.innerHTML=(`${progressValue}%`);
-  if (progressValue === 100) {
-    clearInterval(progress);
-  }
-}, 60);
+  let progressValue = 0;
+  let progressText = document.querySelector(".progressPerc");
+  let progress = setInterval(() => {
+    progressValue++;
+    progressText.innerHTML = `${progressValue}%`;
+    if (progressValue === 100) {
+      clearInterval(progress);
+    }
+  }, 60);
 }
 
 //show link
-function linkView(){
- let linkView = `<div class="linkView">
+function linkView() {
+  let linkView = `<div class="linkView">
  <div id="sample">${link}</div>
  <br />
  <div class="button">
-   <button type="button" id="copyText" onclick="copyText()">
+   <button type="button" id="copyText">
      Copy
    </button>
- </div>`
- dropArea.innerHTML = linkView
+ </div>`;
+  dropArea.innerHTML = linkView;
 }
+// const copyText = document.getElementById("copyText");
 
-function copyText() {
-      
-  /* Copy text into clipboard */
-  navigator.clipboard.writeText
-      ("Geeksforgeeks is best learning platform.");
-}
+document.addEventListener('click', function(e){
+  console.log(e.target.id)
+  if(e.target.id === 'copyText'){
+       //do something
+       e.preventDefault();
+       
+       /* Copy text into clipboard */
+       navigator.clipboard.writeText("This is the best learning platform.");
+  }
+});
